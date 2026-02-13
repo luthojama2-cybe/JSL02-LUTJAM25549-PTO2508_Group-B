683da8681b6c9c2f2745745fed5fb34de3e277d1
Lutho <luthojama2@gmail.com>
2026-02-13 17:38:27 +0200

    completed project

--- scripts.js
+++ scripts.js
@@ -3,52 +3,50 @@ let taskTitle1 = "";
 let taskDescription1 = "";
 let taskStatus1 = "";
 //Task 2 variables
-let tasktitle2 = "";
+let taskTitle2 = "";
 let taskDescription2 = "";
 let taskStatus2 = "";
 
 //Task 1 Function
 function firstTask() {
-    //Task 1 title loop
+    if (taskTitle1 === "" && taskDescription1 === "" && taskStatus1 === ""){
+        console.log("No tasks added, let's get to work!");
+        
+    }
     while (taskTitle1 === ""){
         taskTitle1 = prompt("Enter Task 1 Title")
-    }
-    //Task 1 description loop
-    while (taskDescription1 === ""){
-        taskDescription1 = prompt("Enter Task 1 Discription")
-    }
-    //Task 1 status loop
-    while (taskStatus1 === ""){
+    };
+     while (taskDescription1 === ""){
+        taskDescription1 = prompt("Enter Task 1 Description")
+    };
+     while (taskStatus1 === ""){
         taskStatus1 = prompt("Enter Task 1 Status")
-    }
-    //Task 1 Status Error catching loop
-    if (taskStatus1 !== "Todo" && taskStatus1 !== "Doing" && taskStatus1 !== "Done"){
-        alert("Invalid status: Please enter 'Todo', 'Doing' or 'Done'")
-        taskStatus1 = prompt("Enter Task 1 status")
+    };
+
+    while(taskStatus1 !== "Todo" && taskStatus1 !== "Doing" && taskStatus1 !== "Done"){
+        alert("Invalid Status: Please enter 'Todo', 'Doing' or 'Done'");
+        taskStatus1 = prompt("Enter Task 1 Status")
     }
     console.log(`Title: ${taskTitle1.toLocaleLowerCase()}, Status: ${taskStatus1.toLocaleLowerCase()}`);
 };
 
 //Task 2 function
 function secondTask(){
-    //Task 2 title loop
-     while (tasktitle2 === ""){
-        tasktitle2 = prompt("Enter Task 2 Title")
-    }
-    //Task 2 description loop
-    while (taskDescription2 === ""){
-        taskDescription2 = prompt("Enter Task Discription")
-    }
-    //Task 2 status loop
-    while (taskStatus2 === ""){
+    while (taskTitle2 === ""){
+        taskTitle2 = prompt("Enter Task 2 Title")
+    };
+     while (taskDescription2 === ""){
+        taskDescription2 = prompt("Enter Task 2 Description")
+    };
+     while (taskStatus2 === ""){
         taskStatus2 = prompt("Enter Task 2 Status")
+    };
+
+    while(taskStatus2 !== "Todo" && taskStatus2 !== "Doing" && taskStatus2 !== "Done"){
+        alert("Invalid Status: Please enter 'Todo', 'Doing' or 'Done'");
+        taskStatus2 = prompt("Enter Task 1 Status")
     }
-    //Task 2 status error catching loop
-    if (taskStatus2 !== "Todo" && taskStatus2 !== "Doing" && taskStatus2 !== "Done"){
-        alert("Invalid status: Please enter 'Todo', 'Doing' or 'Done'")
-        taskStatus2 = prompt("Enter task 2 status")
-    }
-    console.log(`Title: ${tasktitle2.toLocaleLowerCase()}, Status: ${taskStatus2.toLocaleLowerCase()}`);
+    console.log(`Title: ${taskTitle2.toLocaleLowerCase()}, Status: ${taskStatus2.toLocaleLowerCase()}`);
 }
 
 firstTask();
